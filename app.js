0a9eba3e11e0a4d373e238c3cf3540cbd06ce81f
Grzegorz Stuczyński <grzegorz.stuczynski@evidenceprime.com>
2019-01-13 19:11:55 +0100

    Added saving score option; hosted image

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const DB_URL = "mongodb://localhost:27017";
 const DB_NAME = "CookieClicker";
 const APP_PORT = 3001;
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/panteon', (req, res) => {
   Score.find()
@@ -21,15 +24,19 @@ app.get('/panteon', (req, res) => {
     });
   });
 
-  app.post('/save-score', (req, res) => {
-    const { score } = req.body;
-    new Score(score)
-      .save()
-      .then(() => res.sendStatus(200))
-      .catch((err) =>{ res.status(500).send(err)
-      console.log(err)
-      })
-    });
+app.post('/save-score', (req, res) => {
+  const { score } = req.body;
+  new Score(score)
+    .save()
+    .then(() => res.sendStatus(200))
+    .catch((err) =>{ res.status(500).send(err)
+    console.log(err)
+    })
+  });
+
+app.get('asset', (req, res)=> {
+
+})
 
 mongoose
   .connect(`${DB_URL}/${DB_NAME}`)
